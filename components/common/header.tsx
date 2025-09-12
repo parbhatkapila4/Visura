8920d73baf5e3d3ba91bf86a2f1f6588967f2df4
parbhatkapila4 <p4parbhatk4kapila@gmail.com>
2025-09-12 16:06:07 +0530

    NavBar

--- components/common/header.tsx
+++ components/common/header.tsx
@@ -1,30 +1,38 @@
-import Link from "next/link";
 import { FileText } from "lucide-react";
+import { Button } from "../ui/button";
+import NavLink from "./nav-link";
 
 export default function Header() {
+  const isLoggedIn = false;
   return (
-    <nav className="container flex justify-between items-center py-4 lg:px-8 px-2 mx-auto">
-      <div className="flex items-center gap-2">
-        <Link href="/" className="flex items-center gap-1 lg:gap-2 shrink-0">
+    <nav className="container flex items-center py-4 lg:px-8 px-2 mx-auto">
+      <div className="flex items-center gap-2 flex-1">
+        <NavLink
+          href="/"
+          className="flex items-center gap-1 lg:gap-2 shrink-0 "
+        >
           <FileText className="w-5 h-5 lg:w-6 lg:h-6 text-gray-900 hover:rotate-12 transform transition duration-200 ease-in-out" />
-          Visura
-        </Link>
+          <span className="font-extrabold lg:text-xl text-gray-900">
+            Visura
+          </span>
+        </NavLink>
       </div>
-      <div className="flex items-center">
-        <Link
-          href="/#pricing"
-          className="text-gray-600 hover:text-gray-900 transition-colors"
-        >
-          Pricing
-        </Link>
+      <div className="flex justify-center gap-4 lg:gap-12 items-center flex-1">
+        <NavLink href="/#pricing">Pricing</NavLink>
+        {isLoggedIn && <NavLink href="/dashboard">Your Summaries</NavLink>}
       </div>
-      <div className="flex items-center">
-        <Link
-          href="/#sign-in"
-          className="text-gray-600 hover:text-gray-900 transition-colors"
-        >
-          Sign In
-        </Link>
+      <div className="flex justify-end items-center flex-1">
+        {isLoggedIn ? (
+          <div className="flex gap-2 items-center">
+            <NavLink href="/upload">Upload a PDF</NavLink>
+            <div>Pro</div>
+            <Button>User</Button>
+          </div>
+        ) : (
+          <div>
+            <NavLink href="/sign-in">Sign In</NavLink>
+          </div>
+        )}
       </div>
     </nav>
   );
