b3e6906df8e71e2c3369638ea404508835d8d939
parbhatkapila4 <p4parbhatk4kapila@gmail.com>
2025-09-12 21:38:35 +0530

    Demo Section with some cool Lucide react icons

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,15 @@
+import BgGradient from "@/components/common/bg-gradient";
+import DemoSection from "@/components/home/demo-section";
 import HeroSection from "@/components/home/hero-section";
 
 export default function Home() {
   return (
     <div className="relative w-full">
-      <HeroSection />
-      {/* <DemoSection /> */}
+      <BgGradient />
+      <div className="flex flex-col ">
+        <HeroSection />
+         <DemoSection />
+      </div>
       {/* <HowItWorksSection /> */}
       {/* <PricingSection /> */}
       {/* <CTASection /> */}
--- components/home/hero-section.tsx
+++ components/home/hero-section.tsx
@@ -9,19 +9,25 @@ export default function HeroSection() {
       className="relative mx-auto flex flex-col z-0 items-center justify-center py-16 sm:py-20 lg:pb-28 
     transition-all animate-in lg:px-12 max-w-7xl"
     >
-      <div className="">
-        <div className="relative p-[1px] overflow-hidden rounded-full bg-gradient-to-r from-rose-200 via-rose-500 to-rose-800 animate-gradient-x group hover:scale-105 transition-transform duration-200">
-          <Badge
-            variant={"secondary"}
-            className="relative px-6 py-2 text-base font-medium bg-white rounded-full group-hover:bg-rose-200 transition-colors duration-200 flex items-center"
-          >
-            <Sparkles className="h-6 w-6 mr-2 text-rose-600 animate-pulse" />
-            <p className="text-base text-rose-600">Powered by AI</p>
-          </Badge>
-        </div>
+      <div className="relative p-[1px] overflow-hidden rounded-full bg-gradient-to-r from-rose-200 via-rose-500 to-rose-800 animate-gradient-x group hover:scale-105 transition-transform duration-200">
+        <Badge
+          variant={"secondary"}
+          className="relative px-6 py-2 text-base font-medium bg-white rounded-full group-hover:bg-rose-200 transition-colors duration-200 flex items-center"
+        >
+          <Sparkles className="h-6 w-6 mr-2 text-rose-600 animate-pulse" />
+          <p className="text-base text-rose-600">Powered by AI</p>
+        </Badge>
       </div>
       <h1 className="font-bold py-6 text-center">
-        Transform PDFs to insights in seconds
+        Transform PDFs to{" "}
+        <span className="relative inline-block">
+          <span className="relative z-10 px-2 ">insights</span>
+          <span
+            className="absolute inset-0 bg-rose-200/50 -rotate-2 rounded-lg transform -skew-y-1"
+            aria-hidden="true"
+          ></span>
+        </span>{" "}
+        in seconds
       </h1>
       <h2 className="text-lg sm:textxl lg:text-2xl text-center px-4 lg:px-0 lg:max-w-4xl text-gray-600">
         Get a sleek summary reel of your document instantly
