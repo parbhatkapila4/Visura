6ca4b6480120918ff84b9d1d8b388e4d4c084a21
parbhatkapila4 <p4parbhatk4kapila@gmail.com>
2025-09-14 17:11:32 +0530

    Upload things api and usage

--- components/upload/upload-form.tsx
+++ components/upload/upload-form.tsx
@@ -1,3 +1,42 @@
+"use client";
+
+import UploadFormInput from "@/components/upload/upload-form-input";
+import { z } from "zod";
+
+const schema = z.object({
+  file: z
+    .instanceof(File, { message: "Invalid file" })
+    .refine(
+      (file) => file.size <= 20 * 1024 * 1024,
+      "File size must be less than 20MB"
+    )
+    .refine(
+      (file) => file.type.startsWith("application/pdf"),
+      "File must be a PDF"
+    ),
+});
+
 export default function UploadForm() {
-  return <div>upload form</div>;
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log("Submitted");
+    const formData = new FormData(e.currentTarget);
+    const file = formData.get("file") as File;
+
+    const validatedFields = schema.safeParse({ file });
+
+    console.log(validatedFields);
+
+    if (!validatedFields.success) {
+      console.log(
+        validatedFields.error.flatten().fieldErrors.file?.[0] ?? "Invalid file"
+      );
+      return;
+    }
+  };
+  return (
+    <div className="flex flex-col gap-8 w-full max-w-2xl mx-auto">
+      <UploadFormInput onSubmit={handleSubmit} />
+    </div>
+  );
 }
